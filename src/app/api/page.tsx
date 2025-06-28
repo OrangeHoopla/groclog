54d23d1398add3daa3febe981d4a023b76dfd026
Your Name <quadekirby@gmail.com>
2025-06-28 18:49:06 -0400

    Disabling cache on api page

--- src/app/api/page.tsx
+++ src/app/api/page.tsx
@@ -1,6 +1,8 @@
 
 import clientPromise from "../../../lib/mongodb";
 
+export const dynamic = "force-dynamic"; 
+export const fetchCache = 'force-no-store';
 
 export default async () => {
 
@@ -9,15 +11,15 @@ export default async () => {
         const client = await clientPromise;
         
         const db = client.db("groclog");
+        
 
-        const movies = await db
-            .collection("Entries")
-            .find({})
+        const entries = await db.collection("Entries")
+        const movies = await entries.find({})
             .limit(10)
             .toArray();
         console.log(movies);
         // return NextResponse.json({ message: "Hello - GET" }, { status: 200 });
-        return movies[0].time
+        return movies[0].time + " " + movies[1].time
 
     } catch (e) {
         console.error(e);
