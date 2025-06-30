c63aefdc878224230934f18545a36e3ea37dfac1
Your Name <quadekirby@gmail.com>
2025-06-29 21:49:15 -0400

    better data presentation

--- src/app/api/page.tsx
+++ src/app/api/page.tsx
@@ -1,29 +1,62 @@
 
-import clientPromise from "../../../lib/mongodb";
+import mongoclient from "../../../lib/mongodb";
 
 export const dynamic = "force-dynamic"; 
 export const fetchCache = 'force-no-store';
 
-export default async () => {
+// interface movie {
+//    _id: string;
+//    time: string;
+// }
 
-    try {
+// const Movies: React.FC<MoviesProps> = ({ movies }) => {
 
-        const client = await clientPromise;
-        
-        const db = client.db("groclog");
-        
+//    return (
+//        <div>
+//            <h1>Top 20 Movies of All Time</h1>
+//            <p>
+//                <small>(According to Metacritic)</small>
+//            </p>
+//            <ul>
+//                {movies.map((movie) => (
+//                    <li key={movie._id}>
+//                        <h2>{movie._id}</h2>
+//                        <h3>{movie.time}</h3>
+//                    </li>
+//                ))}
+//            </ul>
+//        </div>
+//    );
 
-        const entries = await db.collection("Entries")
-        const movies = await entries.find({})
-            .limit(10)
-            .toArray();
-        console.log(movies);
-        // return NextResponse.json({ message: "Hello - GET" }, { status: 200 });
-        return movies[0].time + " " + movies[1].time
+// };
 
-    } catch (e) {
-        console.error(e);
-    }
+// export default Movies;
 
-}
+export const aaaa = async () => {
+   try {
+       const client = await mongoclient;
+       const db = client.db("groclog");
+       const movies = await db
+           .collection("Entries")
+           .find({})
+           .limit(20)
+           .toArray();
+    //    const lala: movie[] = await movies;
+
+       return (
+           <>
+               <ul>
+                   {movies.map(movie => <li key={movie.time }>{ movie.time}</li>)}
+               </ul>
+           </>
+       );
+
+   } catch (e) {
+       console.error(e);
+       return { props: { movies: [] } };
+   }
+
+};
+
+export default aaaa;
 
