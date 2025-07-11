bd7f7a81abeeccf0d53ffa7a7aa27628358ada0c
Your Name <quadekirby@gmail.com>
2025-07-10 22:49:23 -0400

    calls image proc as well

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -6,13 +6,41 @@ export async function POST(req: Request) {
     try {
     const formData = await req.formData();
     // const file = formData.get("file") as File;
-    
     const example = await fetch("http://localhost:8000/api/upload", {
             method: "post",
             body: formData,
+          }).then((res) => {
+            if(!res.ok) {
+                // console.log("Failure:" + res.statusText);
+                throw new Error('HTTP ' + res.status);
+            } else {
+              console.log("Success :" + res.text().then(async (result) => {
+                
+                console.log(result);
+
+                const example2 = await fetch("http://localhost:8000/api/submit/" + result, {
+                  method: "post"
+                }).then((res) => {
+                  if(!res.ok) {
+                      // console.log("Failure:" + res.statusText);
+                      throw new Error('HTTP ' + res.status);
+                  } else {
+                    console.log("Success :" + res.json().then((result) => {
+                      console.log(result);
+                      
+                      }));
+                      // return res.body // what gives?
+                  }
+                });
+
+                }));
+                // return res.body // what gives?
+            }
           });
-        // console.log("success?")
-        console.log(example);
+
+      console.log("-----------------------");
+      
+
     return NextResponse.json({ status: "success for real" });
   } catch (e) {
     console.error(e);
